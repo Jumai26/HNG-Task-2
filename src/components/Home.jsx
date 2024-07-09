df2f1de579eb6c1c09d055f08705c60eb43fc37c
Gloria Jumai Williams <willyabiodun@gmail.com>
2024-07-09 15:44:22 +0100

    added shop page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import ShopPage from "../components/ShopPage"
 
 export default function Home() {
     return (
         <div>
             <Navbar />
+            <ShopPage />
         </div>
     )
 };

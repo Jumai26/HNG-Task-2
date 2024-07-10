1929d6b40a9d5af71193b3dac4b4e49803cc844e
Gloria Jumai Williams <willyabiodun@gmail.com>
2024-07-10 02:12:31 +0100

    mobile resposive checkout done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -13,35 +13,35 @@ export default function Checkout(params) {
             <Navbar />
             <div className="sm:mx-20 mb-3 mx-6 sm:my-20 ">
                 <h1 className='sm:text-4xl sm:leading-10 sm:mb-10'>Checkout</h1>
-                <div className=" grid grid-cols-2 gap-6">
+                <div className=" sm:grid sm:grid-cols-2 flex flex-col gap-6">
                     <div className="">
                         <div className="flex flex-col justify-between content-start">
-                            <p className='text-[9px] sm:text-xl leading-none sm:leading-2 font-medium sm:my-1' >Shipping Details</p>
+                            <p className='text-[12px] mt-9 sm:text-xl leading-none sm:leading-2 font-medium sm:my-1' >Shipping Details</p>
                             <hr className="my-3"></hr>
                             <div className="flex flex-row justify-between">
-                                <p className='text-[9px] sm:text-base leading-none sm:leading-2 font-bold sm:my-1'>Fullstack Mechanic </p>
-                                <p>something@gmail.com</p>
+                                <p className='text-[10px] sm:text-base leading-none sm:leading-2 font-bold sm:my-1'>Fullstack Mechanic </p>
+                                <p className="text-[10px] sm:text-base">something@gmail.com</p>
                             </div>
                             <p className='text-[9px] sm:text-base leading-none font-medium sm:my-1'>14, Afeolusegun Street, <br />Denro Ishasi, Akute, <br />Ojodu Berger.</p>
-                            <p className='text-[9px] sm:text-xl leading-none sm:leading-2 font-medium sm:my-1 pt-16'>Payment Details</p>
+                            <p className='text-[12px] sm:text-xl leading-none sm:leading-2 font-medium sm:my-1 pt-16'>Payment Details</p>
                             <hr className="my-3"></hr>
                             <form method="post" className="container ">
                                 <div className='flex flex-col justify-start pt-4'>
-                                    <label id="name" for="name">Name on card</label>
-                                    <input id="name" type="text" placeholder="e.g Fullstack Mechanic" className='border rounded-xl border-gray-600 h-14 my-1 placeholder:pl-3 ' required />
+                                    <label id="name" for="name" className="text-[12px] sm:text-base">Name on card</label>
+                                    <input id="name" type="text" placeholder="e.g Fullstack Mechanic" className='border rounded-xl border-gray-600 h-10 sm:h-14 my-1 placeholder:pl-3 placeholder:text-[12px] placeholder:sm:text-base' required />
                                 </div>
                                 <div className='flex flex-col justify-start pt-6'>
-                                    <label id="email" for="email"> Card Number</label>
-                                    <input id="email" type="number" placeholder="e.g 123 45 678" className='border rounded-xl border-gray-600 h-14 my-1 placeholder:pl-3 ' required />
+                                    <label className="text-[12px] sm:text-base"> Card Number</label>
+                                    <input id="email" type="number" placeholder="e.g 123 45 678" className='border rounded-xl border-gray-600 h-10 sm:h-14 my-1 placeholder:pl-3 placeholder:text-[12px] placeholder:sm:text-base ' required />
                                 </div>
                                 <div className='grid grid-cols-2 gap-6 pt-6'>
                                     <div className='flex flex-col justify-start '>
-                                        <label id="email" for="email"> Exp. Date</label>
-                                        <input id="email" type="number" placeholder="e.g 10/10/2005" className='border rounded-xl border-gray-600 h-14 my-1 placeholder:pl-3 ' required />
+                                        <label className="text-[12px] sm:text-base" > Exp. Date</label>
+                                        <input id="email" type="number" placeholder="e.g 10/10/2005" className='border rounded-xl border-gray-600 h-10 sm:h-14 my-1 placeholder:pl-3 placeholder:text-[12px] placeholder:sm:text-base' required />
                                     </div>
                                     <div className='flex flex-col justify-start '>
-                                        <label id="email" for="email"> CVV</label>
-                                        <input id="email" type="number" placeholder="e.g 001" className='border rounded-xl border-gray-600 h-14 my-1 placeholder:pl-3 ' required />
+                                        <label className="text-[12px] sm:text-base"> CVV</label>
+                                        <input id="email" type="number" placeholder="e.g 001" className='border rounded-xl border-gray-600 h-10 sm:h-14my-1 placeholder:pl-3 placeholder:text-[12px] placeholder:sm:text-base' required />
                                     </div>
                                 </div>
                                 <div>
@@ -59,7 +59,7 @@ export default function Checkout(params) {
                             <div className=" flex flex-row justify-between content-start ">
                                 <div className="flex flex-row gap-x-4">
                                     <div>
-                                        <img src={Shoeone} className=' w-[77px] h-[45px] sm:w-[127px] sm:h-[100px] p-5 bg-[#F3F4F3] rounded-xl m-auto'></img>
+                                        <img src={Shoeone} className=' w-[127px] h-[80px] sm:w-[127px] sm:h-[100px] p-2 sm:p-5 bg-[#F3F4F3] rounded-xl m-auto'></img>
                                     </div>
                                     <div className="">
                                         <p className='text-[9px] sm:text-sm leading-none sm:leading-4 font-medium sm:my-2'>Nike Running Shoe</p>
@@ -81,7 +81,7 @@ export default function Checkout(params) {
                             <div className=" flex flex-row justify-between content-start ">
                                 <div className="flex flex-row gap-x-4">
                                     <div>
-                                        <img src={Shoeseven} className=' w-[77px] h-[45px] sm:w-[127px] sm:h-[100px] p-5 bg-[#F3F4F3] rounded-xl m-auto'></img>
+                                        <img src={Shoeseven} className=' w-[127px] h-[80px]  sm:w-[127px] sm:h-[100px] p-2 sm:p-5 bg-[#F3F4F3] rounded-xl m-auto'></img>
                                     </div>
                                     <div className="">
                                         <p className='text-[9px] sm:text-sm leading-none sm:leading-4 font-medium sm:my-2'>Nike Running Shoe</p>
@@ -103,7 +103,7 @@ export default function Checkout(params) {
                             <div className=" flex flex-row justify-between content-start  ">
                                 <div className="flex flex-row gap-x-4">
                                     <div>
-                                        <img src={Shoeeight} className=' w-[77px] h-[45px] sm:w-[127px] sm:h-[100px] p-5 bg-[#F3F4F3] rounded-xl m-auto'></img>
+                                        <img src={Shoeeight} className=' w-[127px] h-[80px] p-2 sm:w-[127px] sm:h-[100px] sm:p-5 bg-[#F3F4F3] rounded-xl m-auto'></img>
                                     </div>
                                     <div className="">
                                         <p className='text-[9px] sm:text-sm leading-none sm:leading-4 font-medium sm:my-2'>Nike Running Shoe</p>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import Instagram from '../assets/icons/instagram.png'
 
 export default function Footer(params) {
     return (
-        <div className='m-0 font-body font-normal text-sm bg-black text-white'>
-            <div className="flex justify-center gap-10 sm:pt-10 ">
+        <div className='m-0 font-body font-normal text-[9px] sm:text-sm bg-black text-white'>
+            <div className="flex justify-center gap-3 sm:gap-10 sm:pt-10 ">
                 <p>About</p>
                 <p> Blog</p>
                 <p>Job</p>
@@ -13,12 +13,12 @@ export default function Footer(params) {
                 <p>Accessibility</p>
                 <p>Partners</p>
             </div>
-            <div  className="flex justify-center gap-10 sm:pt-10 ">
-                <img src={Facebook} ></img>
-                <img src={Twitter} ></img>
-                <img src={Instagram} ></img>
+            <div  className="flex justify-center gap-3 sm:gap-10 pt-3 sm:pt-10 ">
+                <img src={Facebook} className='w-3 h-3 sm:w-5 sm:h-5'></img>
+                <img src={Twitter} className='w-3 h-3 sm:w-5 sm:h-5'></img>
+                <img src={Instagram} className='w-3 h-3 sm:w-5 sm:h-5' ></img>
             </div>
-            <div className="flex justify-center gap-x-10 sm:py-10 ">© 2024 Fullstack Mechanic. All rights reserved</div>
+            <div className="flex justify-center gap-x-10 py-3 sm:py-10 ">© 2024 Fullstack Mechanic. All rights reserved</div>
         </div>
     )
 };
